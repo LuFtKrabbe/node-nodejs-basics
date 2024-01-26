0ed3980f2ea722930ac39aa80779c4ff6b4aae5d
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2024-01-26 22:26:25 +0500

    feat: cope with stream tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { join } from 'node:path';
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const basePath = './src/streams/files';
+    const fileToReadName = 'fileToRead.txt';
+
+    const readStream = createReadStream(join(basePath, fileToReadName));
+    const writableToTerminal = process.stdout;
+
+    let readContent = '';
+
+    readStream.on('data', (chunk) => {
+        readContent += chunk.toString();
+    });
+
+    readStream.on('end', () => {
+        writableToTerminal.write(readContent);
+        writableToTerminal.write('\n');
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { join } from 'node:path';
+import { createWriteStream } from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+    const basePath = './src/streams/files';
+    const fileToWriteName = 'fileToWrite.txt';
+
+    const readableFromTerminal = process.stdin;
+    const writeStream = createWriteStream(join(basePath, fileToWriteName));
+
+    console.log('Enter your data to write in the File:');
+    readableFromTerminal.on('data', chunk => {
+        writeStream.write(chunk.toString());
+    })
 };
 
 await write();
