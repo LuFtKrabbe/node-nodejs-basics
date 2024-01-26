a48724db869f925c80344afd7887b882c8e03578
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2024-01-26 17:36:05 +0500

    feat: cope with hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { join } from 'node:path';
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const basePath = './src/hash/files';
+    const fileName = 'fileToCalculateHashFor.txt';
+    const algorithm = 'sha256';
+
+    const hash = createHash(algorithm);
+    const stream = createReadStream(join(basePath, fileName));
+
+    stream.on('readable', () => {
+        const data = stream.read();
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(`${hash.digest('hex')}`);
+        }
+    });
 };
 
 await calculateHash();
