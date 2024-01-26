5e83f22aa2d5de3c8240f3ad04d2afbf31bf6266
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2024-01-26 22:59:12 +0500

    feat: cope with zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { join } from 'node:path';
+import { rm } from 'node:fs/promises';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const basePath = './src/zip/files';
+    const fileToCompress = 'fileToCompress.txt';
+    const fileCompressed = 'archive.gz';
+
+    try {
+        await pipeline(
+          createReadStream(join(basePath, fileToCompress)),
+          createGzip(),
+          createWriteStream(join(basePath, fileCompressed)),
+        );
+        await rm(join(basePath, fileToCompress));
+      } catch (err) {
+        console.error(err);
+      }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { join } from 'node:path';
+import { rm } from 'node:fs/promises';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const basePath = './src/zip/files';
+    const fileToDecompressed = 'archive.gz';
+    const fileToCompress = 'fileToCompress.txt';
+
+    try {
+        await pipeline(
+          createReadStream(join(basePath, fileToDecompressed)),
+          createUnzip(),
+          createWriteStream(join(basePath, fileToCompress)),
+        );
+        await rm(join(basePath, fileToDecompressed));
+      } catch (err) {
+        console.error(err);
+      }
 };
 
 await decompress();
