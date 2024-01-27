0ada1f17f6bc7d9f459a035656fd884f8e4fcf68
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2024-01-27 18:56:19 +0500

    feat: cope with wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import os from 'node:os'
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
-};
+    const initValue = 10;
+    const qtyCPU = os.cpus().length;
+    const resArr = [];
+
+    for (let i = 0; i < qtyCPU; i++) {
+        resArr.push(new Promise((resolve) => {
+            const worker = new Worker('./src/wt/worker.js', { workerData: initValue + i });
+            worker.on('message', (message) => {
+                resolve({status: 'resolved', value: message})
+            });
+            worker.on('error', () => {
+                resolve({status: 'error', value: null})
+            });
+        }))
+    }
+
+    console.log(await Promise.all(resArr));
+}
 
 await performCalculations();
